7d922233179a855657206c8ce1f2e96bc4bb2a4e
dlehddud60 <dlehddud60@naver.com>
2023-04-28 14:39:28 +0900

    firstCommit MVC07

---
+++ jee-2023-03/SpringMVC066/src/main/java/kr/board/entity/Board.java
@@ -0,0 +1,19 @@
+package kr.board.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @AllArgsConstructor @NoArgsConstructor
+public class Board {
+    private int idx;
+    private String memID; //회원ID
+    private String title;
+    private String content;
+    private String writer;
+    private String indate; //작성일
+    private int count;
+
+
+
+}
---
+++ jee-2023-03/SpringMVC066/src/main/webapp/WEB-INF/views/access-denied.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: leedongyoung
+  Date: 2023/04/27
+  Time: 8:27 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  $END$
+  </body>
+</html>
